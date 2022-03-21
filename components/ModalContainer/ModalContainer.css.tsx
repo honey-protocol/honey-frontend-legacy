d87f963982ac7e9b9bca71a5d9c868060b7ea71f
Oshioke Iyogwoya <danieloshos3@gmail.com>
2022-03-21 17:07:28 +0100

    Added modal container component

---
+++ components/ModalContainer/ModalContainer.css.tsx
@@ -0,0 +1,17 @@
+import { style } from '@vanilla-extract/css';
+
+export const modalContainer = style({
+  display: 'flex',
+  position: 'fixed',
+  overflow: 'auto',
+  cursor: 'pointer',
+  padding: '5rem 0px 0px',
+  inset: '0',
+  zIndex: '99',
+  background:
+    'linear-gradient(to right, rgba(var(--background), 0.5), rgba(var(--background), 0.5)),linear-gradient( to right, rgba(var(--foreground), 0.1), rgba(var(--foreground), 0.1) )',
+  backdropFilter: 'blur(5px)',
+  transform: 'translateZ(0px)',
+  justifyContent: 'center',
+  alignItems: 'center'
+});
