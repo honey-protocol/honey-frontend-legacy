55bc80fd7bdeeee4fad4f21b7064db7348271082
BowTiedSwan <0xf85ce443D3F031728e0e97503C06322c2F51A0c7@ethmail.cc>
2022-03-23 18:03:20 +0200

    Add styling file

---
+++ styles/loan.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css';
+import { sprinkles, vars } from './theme.css';
+
+export const vaultsList = style({
+  overflowX: 'hidden',
+  overflowY: 'auto',
+  maxHeight: '250px'
+});
+
+
