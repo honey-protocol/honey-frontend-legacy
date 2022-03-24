cd2c4b7fceae4fa717337150976def67badab5ad
BowTiedFirefox <BowTiedFirefox@protonmail.com>
2022-03-24 08:06:29 -0400

    set up env file for goki

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
-const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+const {createVanillaExtractPlugin} = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const {PHASE_DEVELOPMENT_SERVER} = require('next/constants')
 
 /** @type {import('next').NextConfig} */
-
-module.exports = (phase, { defaultConfig }) => {
+const mainNetEndpoint = process.env.NEXT_PUBLIC_RPC_NODE;
+module.exports = (phase, {defaultConfig}) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     const env = {
-      NETWORK: 'devnet'
+      NETWORK: 'devnet',
+      NETWORK_CONFIGURATION: undefined
     }
 
     const devNextConfig = {
@@ -15,13 +16,19 @@ module.exports = (phase, { defaultConfig }) => {
       env: env,
     };
     return withVanillaExtract(devNextConfig)
-  }else {
+  } else {
     const env = {
-      NETWORK: "mainnet-beta"
+      NETWORK: "mainnet-beta",
+      NETWORK_CONFIGURATION: {
+        'mainnet-beta': {
+          name: 'mainnet-beta',
+          endpoint: mainNetEndpoint,
+        }
+      }
     }
     const ProdNextConfig = {
       reactStrictMode: true,
-      env : env,
+      env: env,
     };
     return withVanillaExtract(ProdNextConfig)
   }
