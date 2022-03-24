18fde85545a2370d12a9bee3c66e9536d4b1bf28
BowTiedSwan <0xf85ce443D3F031728e0e97503C06322c2F51A0c7@ethmail.cc>
2022-03-24 14:21:05 +0200

    Add template components for deposit withdraw module

---
+++ components/DepositWithdrawModule/DepositWIthdrawModule.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Box, Button, Card, Stack, Text, Tag } from 'degen';
+import { Avatar } from 'degen';
+import { Input } from 'degen'
+import LoanDeposit from '../LoanDeposit';
+import LoanWithdraw from '../LoanWithdraw';
+import * as styles from './DepositWithdrawModule.css';
+import ToggleSwitchLoan from '../ToggleSwitchLoan';
+
+type TButton = {
+  title: string;
+  disabled: boolean;
+  hidden?: boolean;
+  onClick: () => void;
+};
+interface DepositWithdrawModuleProps {
+  // isFetching: boolean;
+  // NFTs: NFT[];
+  // selectedNFTs: NFT[];
+  // title: string;
+  // buttons: TButton[];
+  // onNFTSelect: Function | null;
+  // onNFTUnselect: (NFT: NFT) => void;
+}
+
+const DepositWithdrawModule = (props: DepositWithdrawModuleProps) => {
+  const {
+    // NFTs,
+    // selectedNFTs,
+    // title,
+    // buttons,
+    // onNFTSelect,
+    // onNFTUnselect,
+    // isFetching
+  } = props;
+
+  const [depositOrWithdraw, setDepositOrWithdraw] = useState(0);
+
+  return (
+    <Box className={styles.cardContainer}>
+      <Card level="2" width="full" padding="8" shadow>
+        <ToggleSwitchLoan
+          buttons={[
+            {
+              title: 'Deposit',
+              onClick: () => setDepositOrWithdraw(0)
+            },
+            { title: 'Widthdraw', onClick: () => setDepositOrWithdraw(1) }
+          ]}
+          activeIndex={depositOrWithdraw}
+        />
+        <hr></hr>
+        {
+          depositOrWithdraw == 0  ? (
+            <LoanDeposit
+              borrowApy={1}
+              estValue={1}
+              assetsBorrowed={1}
+              netBorrowBalance={1}
+
+            />
+            ) : (
+              <LoanWithdraw
+                nftName={'test'}
+                evaluation={1}
+                interestRate={1}
+                assetsBorrowed={1}
+                totalInterest={1}
+                totalPayback={1}
+              />
+            )
+        };
+      </Card>
+    </Box>
+  );
+};
+
+export default DepositWithdrawModule;
---
+++ components/LoanDeposit.tsx
@@ -0,0 +1,211 @@
+import React from "react";
+import { Box, Button, Card, Stack, Text, Tag } from 'degen';
+import { Avatar } from 'degen';
+import { Input } from 'degen'
+
+interface LoanDepositProps {
+    borrowApy: number,
+    estValue: number,
+    assetsBorrowed: number,
+    netBorrowBalance: number,
+}
+
+const LoanDeposit = (props: LoanDepositProps) => {
+    const {
+        borrowApy,
+        estValue,
+        assetsBorrowed,
+        netBorrowBalance
+    } = props;
+
+    return (
+        <Box gap="3">
+            {/* Vault data row */}
+            <Stack
+            justify="space-between"
+            >
+            <Stack
+                direction="horizontal"
+                justify="space-between"
+                align="center"
+            >
+                <Box alignItems="flex-start">
+                    <Avatar label="" size="10" src={""} />
+                </Box>
+                <Box
+                    paddingBottom="2"
+                >
+                    <Stack
+                        direction="horizontal"
+                        justify="space-between"
+                        align="center"
+                        space="2"
+                    >
+                        <Text
+                            align="right"
+                            weight="semiBold"
+                            color="foreground"
+                            variant="large"
+                        >
+                        NFT name #234
+                        </Text>
+                    </Stack>
+                </Box>
+            </Stack>
+            </Stack>
+            <hr></hr>
+            {/* Liquidation and interest data */}
+            <Box
+            paddingTop="1"
+            paddingBottom="1"
+            >
+                <Stack
+                    justify="space-between"
+                >
+                    <Stack
+                    direction="horizontal"
+                    justify="space-between"
+                    align="center"
+                    space="2"
+                    >
+                        <Text
+                            align="left"
+                            color="textSecondary">
+                            Borrow APY
+                        </Text>
+                        <Text
+                            align="right"
+                            color="foreground"
+                        >
+                            4.2%
+                        </Text>
+                    </Stack>
+                    <Stack
+                    direction="horizontal"
+                    justify="space-between"
+                    align="center"
+                    space="2"
+                    >
+                        <Text align="left"
+                        color="textSecondary">Estimated value</Text>
+                        <Text
+                            align="right"
+                            color="foreground"
+                        >
+                            $4,500
+                        </Text>
+                    </Stack>
+                </Stack>
+            </Box>
+            <hr></hr>
+            {/* Assets borrowed */}
+            <Box
+                paddingTop="1"
+                paddingBottom="1"
+            >
+                <Stack
+                    justify="space-between"
+                >
+                    <Stack
+                        direction="horizontal"
+                        justify="space-between"
+                        align="center"
+                        space="2"
+                    >
+                        <Text
+                            align="left"
+                            color="textSecondary">
+                                Assets borrowed
+                        </Text>
+                        <Text
+                            align="right"
+                            color="foreground"
+                        >
+                            $782.5
+                        </Text>
+                    </Stack>
+                    <Stack
+                        direction="horizontal"
+                        justify="space-between"
+                        align="center"
+                        space="2"
+                    >
+                        <Stack direction="horizontal">
+                            <Text
+                                align="left"
+                                color="foreground"
+                            >
+                            782.5 USDC
+                            </Text>
+                        </Stack>
+                    <Text
+                        align="right"
+                        color="foreground"
+                    >
+                        $782.5
+                    </Text>
+                    </Stack>
+                </Stack>
+            </Box>
+            <hr></hr>
+            {/* Interest & payback data*/}
+            <Box
+                paddingTop="1"
+                paddingBottom="3"
+            >
+                <Stack
+                direction="horizontal"
+                justify="space-between"
+                align="center"
+                space="2"
+                >
+                    <Text
+                        align="left"
+                        color="textSecondary">
+                        Net borrow balance
+                    </Text>
+                    <Text
+                        align="right"
+                        color="foreground"
+                    >
+                        $2,500
+                    </Text>
+                </Stack>
+            </Box>
+            {/* Borrowed amount and currency */}
+            <Box
+                paddingTop="5"
+            >
+            <Input
+                hideLabel
+                label="Amount"
+                max={100}
+                min={0}
+                placeholder="20"
+                type="number"
+                units="SOL"
+            />
+            </Box>
+            <Box
+                height="16"
+                paddingTop="4"
+            >
+            {/* <Range /> */}
+            <Stack
+                direction="horizontal"
+                align="center"
+                justify="space-around"
+            >
+                <Text align="left">0%</Text>
+                <Text align="center">25%</Text>
+                <Text align="center">50%</Text>
+                <Text align="center">75%</Text>
+                <Text align="right">100%</Text>
+            </Stack>
+            </Box>
+            <Button width="full">Borrow</Button>
+        </Box>
+    )
+}
+
+export default LoanDeposit;
