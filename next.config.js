e8a366066c44e7d435dd935321400435dd667caa
BowTiedFirefox <BowTiedFirefox@protonmail.com>
2022-03-24 10:19:44 -0400

    add comments

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const {PHASE_DEVELOPMENT_SERVER} = require('next/constants')
 
 /** @type {import('next').NextConfig} */
 const mainNetEndpoint = process.env.NEXT_PUBLIC_RPC_NODE;
+
+/** We should put all environment dependent variables into this file. However, Prod RPC NODE should still reside in
+ * .env file for security reason. API Keys and secrets should also reside in .env file
+ * "yarn dev" is dev build so by default should use the settings related to  dev-net
+ * "yarn build/yarn start" is prod build so by default should use settings related to Mainnet
+ * */
+
 module.exports = (phase, {defaultConfig}) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     const env = {
