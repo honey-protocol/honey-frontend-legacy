d313e9b3a9c56ba4e45e2aaf73c06bc3a0c8734a
Oshioke Iyogwoya <danieloshos3@gmail.com>
2022-03-25 13:47:02 +0100

    added vesting period select

--- components/VeHoneyModal.tsx
+++ components/VeHoneyModal.tsx
@@ -1,7 +1,24 @@
 import { Box, Button, Input, Stack, Text } from 'degen';
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import * as styles from './VeHoneyModal.css';
 
 const VeHoneyModal = () => {
+  const [amount, setAmount] = useState<number>();
+  const [vestingPeriod, setVestingPeriod] = useState('3 months');
+  const veHoneyRewardRate =
+    vestingPeriod === '3 months'
+      ? '2'
+      : vestingPeriod === '6 months'
+      ? '5'
+      : vestingPeriod === '12 months'
+      ? '10'
+      : '1';
+
+  const onSubmit = () => {
+    if (!amount) return;
+    console.log({ amount, vestingPeriod });
+  };
+
   return (
     <Box width="96">
       <Box borderBottomWidth="0.375" paddingX="6" paddingY="4">
@@ -26,25 +43,55 @@ const VeHoneyModal = () => {
             width="3/4"
           >
             <Text variant="small" color="accent">
-              10 pHONEY = 3.142 veHONEY
+              1 pHONEY = {veHoneyRewardRate} veHONEY
             </Text>
           </Box>
           <Stack space="2">
             <Stack direction="horizontal" justify="space-between">
               <Text variant="small" color="textSecondary">
-                Your veHoney deposited
+                Your pHoney deposited
               </Text>
-              <Text variant="small">332,420</Text>
+              <Text variant="small">--</Text>
             </Stack>
             <Stack direction="horizontal" justify="space-between">
               <Text variant="small" color="textSecondary">
                 Your pHONEY balance
               </Text>
-              <Text variant="small">332,420</Text>
+              <Text variant="small">--</Text>
+            </Stack>
+            <Stack direction="horizontal" justify="space-between">
+              <Text variant="small" color="textSecondary">
+                Vesting period
+              </Text>
+              <Box>
+                <select
+                  name="vestingPeriod"
+                  value={vestingPeriod}
+                  className={styles.select}
+                  onChange={event => setVestingPeriod(event.target.value)}
+                >
+                  <option value="3 months">3 months</option>
+                  <option value="6 months">6 months</option>
+                  <option value="12 months">12 months</option>
+                </select>
+              </Box>
             </Stack>
           </Stack>
-          <Input type="number" label="Amount" hideLabel placeholder="0" />
-          <Button width="full">Deposit</Button>
+          <Input
+            value={amount}
+            type="number"
+            label="Amount"
+            hideLabel
+            placeholder="0"
+            onChange={event => setAmount(Number(event.target.value))}
+          />
+          <Button
+            onClick={onSubmit}
+            disabled={amount ? false : true}
+            width="full"
+          >
+            {amount ? 'Deposit' : 'Enter amount'}
+          </Button>
         </Stack>
       </Box>
     </Box>
