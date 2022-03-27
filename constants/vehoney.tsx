533deb7471f6488e28e14506abcbb90a99ae2161
Gonzalo Astorga <gonzalo.astorgaie@gmail.com>
2022-03-27 05:01:02 +0200

    feat: added idls, types and constants

---
+++ constants/vehoney.tsx
@@ -0,0 +1,6 @@
+export const LOCKER_SEED = "Locker";
+export const ESCROW_SEED = "Escrow";
+export const WHITELIST_ENTRY_SEED = "LockerWhitelistEntry";
+
+export const TOKEN_VAULT_SEED = "TokenVault";
+export const VAULT_AUTHORITY_SEED = "VaultAuthority";
