5046ac79f00aa4f43fcaf21dcd8e3ad9be2d3e39
Gonzalo Astorga <gonzalo.astorgaie@gmail.com>
2022-03-27 04:48:47 +0200

    wip: idl missing program id

--- components/VeHoneyModal/VeHoneyModal.tsx
+++ components/VeHoneyModal/VeHoneyModal.tsx
@@ -1,6 +1,69 @@
 import { Box, Button, Input, Stack, Text } from 'degen';
 import React, { ChangeEvent, useState } from 'react';
 import * as styles from './VeHoneyModal.css';
+import * as idl from "../../idl/ve_honey.json"
+import * as anchor from "@project-serum/anchor";
+import { web3, Program } from '@project-serum/anchor'
+import { Connection, PublicKey, clusterApiUrl} from "@solana/web3.js"
+import { ASSOCIATED_TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
+import type { VeHoney } from "../../types/ve_honey";
+import { Stake } from "../../types/stake";
+import * as constants from "../../constants/vehoney";
+// TODO best find way to import IDLs currently is done with nodejs fs. Find out if needed since you alrady have program ID
+import { useConnectedWallet, useSolana , useConnection} from '@saberhq/use-solana';
+
+const { SystemProgram, Keypair} = web3;
+
+const programID = new PublicKey(idl.metadata.address)
+console.log(programID)
+
+const programId = new anchor.web3.PublicKey(
+  "CKQapf8pWoMddT15grV8UCPjiLCTHa12NRgkKV63Lc7q"
+);
+const clusterUrl = "https://api.devnet.solana.com";
+
+
+// const connection = new anchor.web3.Connection(clusterUrl, "processed");
+// const provider = new anchor.Provider(connection, new anchor.Wallet(payer), {
+//   skipPreflight: false,
+//   preflightCommitment: "processed",
+//   commitment: "processed",
+// });
+// anchor.setProvider(provider);
+
+// const program = anchor.workspace.VeHoney as Program<VeHoney>;
+// const publicConnection = new anchor.web3.Connection(clusterUrl, {
+//   commitment: "processed",
+// });
+
+// const SYSTEM_PROGRAM = anchor.web3.SystemProgram.programId;
+// const TOKEN_PROGRAM_ID = anchor.Spl.token().programId;
+// const LAMPORTS_PER_SOL = anchor.web3.LAMPORTS_PER_SOL;
+
+// const admin = anchor.web3.Keypair.generate(); // TODO: save this key, and load  on runtime
+// const user = anchor.web3.Keypair.generate(); // TODO: save this key, and load on runtime
+// let honeyMint: Token;
+// let pHoneyMint: Token;
+// const honeyMintAuthority = anchor.web3.Keypair.generate(); // TODO: save this key, and load  on runtime
+// const pHoneyMintAuthority = anchor.web3.Keypair.generate(); // TODO: save this key, and load  on runtime
+// const base = anchor.web3.Keypair.generate(); // TODO: save this key, and load  on runtime
+// let userPHoneyToken: anchor.web3.PublicKey,
+//   userHoneyToken: anchor.web3.PublicKey,
+//   locker: anchor.web3.PublicKey,
+//   escrow: anchor.web3.PublicKey,
+//   lockedTokens: anchor.web3.PublicKey,
+//   whitelistEntry: anchor.web3.PublicKey;
+// const lockerParams = {
+//   whitelistEnabled: true,
+//   minStakeDuration: new anchor.BN(1),
+//   maxStakeDuration: new anchor.BN(5),
+//   multiplier: 48,
+// };
+// const stakeProgram = anchor.workspace.Stake as Program<Stake>;
+// let tokenVault: anchor.web3.PublicKey,
+//   tokenVaultBump: number,
+//   vaultAuthority: anchor.web3.PublicKey,
+//   vaultAuthorityBump: number;
 
 const VeHoneyModal = () => {
   const [amount, setAmount] = useState<number>();
@@ -18,6 +81,18 @@ const VeHoneyModal = () => {
     if (!amount) return;
     console.log({ amount, vestingPeriod });
   };
+  // const payer = anchor.web3.Keypair.generate();
+  const wallet = useConnectedWallet();
+
+  const connection = useConnection();
+  const provider = new anchor.Provider(connection,  wallet , {
+    skipPreflight: false,
+    preflightCommitment: "processed",
+    commitment: "processed",
+  });
+  console.log("Payer", wallet)
+  console.log("Connection", connection)
+  console.log("Provider", provider)
 
   return (
     <Box width="96">
