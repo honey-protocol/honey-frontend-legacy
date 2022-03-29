36501582145b87bdde23b3e85cef8bb6083bc0f8
gonwi <gonzalo.astorgaie@gmail.com>
2022-03-29 10:00:42 +0200

    wip: sdk pdas

--- helpers/sdk/vehoney.pda.ts
+++ helpers/sdk/vehoney.pda.ts
@@ -1,53 +1,35 @@
+import { PublicKey } from '@solana/web3.js';
+import { STAKE_PROGRAM_ID } from './index';
+import { VE_HONEY_PROGRAM_ID } from './index';
+
 let lockerBump: number;
 [locker, lockerBump] = await anchor.web3.PublicKey.findProgramAddress(
   [Buffer.from(constants.LOCKER_SEED), base.publicKey.toBuffer()],
   program.programId
 );
 
-import { PublicKey } from '@solana/web3.js';
-import { GEM_BANK_PROG_ID } from '../../index';
+
+userHoneyToken = await Token.getAssociatedTokenAddress(
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+  honeyMint.publicKey,
+  user.publicKey
+);
 
 
-// TODO : import PTA
+[escrow, escrowBump] = await anchor.web3.PublicKey.findProgramAddress(
+  [
+    Buffer.from(constants.ESCROW_SEED),
+    locker.toBuffer(),
+    user.publicKey.toBuffer(),
+  ],
+  program.programId
+);
 
-export const findVaultPDA = async (bank: PublicKey, creator: PublicKey) => {
-  return PublicKey.findProgramAddress(
-    [Buffer.from('vault'), bank.toBytes(), creator.toBytes()],
-    GEM_BANK_PROG_ID
-  );
-};
-
-export const findGemBoxPDA = async (vault: PublicKey, mint: PublicKey) => {
-  return PublicKey.findProgramAddress(
-    [Buffer.from('gem_box'), vault.toBytes(), mint.toBytes()],
-    GEM_BANK_PROG_ID
-  );
-};
-
-export const findGdrPDA = async (vault: PublicKey, mint: PublicKey) => {
-  return PublicKey.findProgramAddress(
-    [Buffer.from('gem_deposit_receipt'), vault.toBytes(), mint.toBytes()],
-    GEM_BANK_PROG_ID
-  );
-};
-
-export const findVaultAuthorityPDA = async (vault: PublicKey) => {
-  return PublicKey.findProgramAddress([vault.toBytes()], GEM_BANK_PROG_ID);
-};
-
-export const findWhitelistProofPDA = async (
-  bank: PublicKey,
-  whitelistedAddress: PublicKey
-) => {
-  return PublicKey.findProgramAddress(
-    [Buffer.from('whitelist'), bank.toBytes(), whitelistedAddress.toBytes()],
-    GEM_BANK_PROG_ID
-  );
-};
-
-export const findRarityPDA = async (bank: PublicKey, mint: PublicKey) => {
-  return PublicKey.findProgramAddress(
-    [Buffer.from('gem_rarity'), bank.toBytes(), mint.toBytes()],
-    GEM_BANK_PROG_ID
-  );
-};
+lockedTokens = await Token.getAssociatedTokenAddress(
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+  honeyMint.publicKey,
+  escrow,
+  true
+);
