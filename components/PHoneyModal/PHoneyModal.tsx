28d37f1e1ab90fd793bc83d1d66df021720ebc05
Gonzalo Astorga <gonzalo.astorgaie@gmail.com>
2022-04-02 07:40:51 +0200

    feat: enviroment variable config

--- components/PHoneyModal/PHoneyModal.tsx
+++ components/PHoneyModal/PHoneyModal.tsx
@@ -13,14 +13,8 @@ const PHoneyModal = () => {
 
   // ======================== Should replace with configuration ================
   const pHoneyToken = tokenAccounts.find(t => t.info.mint.equals(PHONEY_MINT));
-  const STAKE_POOL_ADDRESS = new PublicKey(
-    process.env.NEXT_STAKE_POOL_ADDR ||
-      '4v62DWSwrUVEHe2g88MeyJ7g32vVzQsCnADZF8yUy8iU'
-  );
-  const LOCKER_ADDRESS = new PublicKey(
-    process.env.NEXT_LOCKER_ADDR ||
-      '5FnK8H9kDbmPNpBYMuvSkDevkMfnVPRrPNNqmTQyBBae'
-  );
+  const STAKE_POOL_ADDRESS = new PublicKey(process.env.NEXT_PUBLIC_STAKE_POOL_ADDRESS);
+  const LOCKER_ADDRESS = new PublicKey(process.env.NEXT_PUBLIC_LOCKER_ADDRESS);
   // ============================================================================
 
   const { user, deposit, claim } = useStake(STAKE_POOL_ADDRESS, LOCKER_ADDRESS);
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,25 @@
 declare module '*.png';
 declare module '*.svg';
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      // ===== Input new enviroment variables =====
+      // ===== from .env here =====================
+      NEXT_PUBLIC_RPC_NODE: string;
+      NEXT_PUBLIC_STAKE_POOL_ADDRESS: string;
+      NEXT_PUBLIC_LOCKER_ADDRESS: string;
+      NEXT_PUBLIC_HONEY_MINT: string;
+      NEXT_PUBLIC_PHONEY_MINT: string;
+      NEXT_PUBLIC_WHITELIST_ENTRY: string;
+      NEXT_PUBLIC_STAKE_PROGRAM_ID: string;
+      NEXT_PUBLIC_VE_HONEY_PROGRAM_ID: string;
+      // ==========================================
+      NODE_ENV: 'development' | 'production';
+      PORT?: string;
+      PWD: string;
+    }
+  }
+}
 
 type Creator = {
   address: string;
@@ -17,3 +37,5 @@ type NFT = {
   tokenId: string;
   mint: string;
 };
+
+export {}
