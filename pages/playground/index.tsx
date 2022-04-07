2f0633e53174b4e9511930f73581845ca7f4c874
Brian Broeking <bbroeking456@gmail.com>
2022-04-06 23:58:32 -0500

    get the wrapper to build

--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -1,11 +1,11 @@
-import { useConnection, useMarket } from '@honey-defi/sdk'
-import { useConnectedWallet } from '@saberhq/use-solana';
+import { useMarket } from '@honey-defi/sdk'
+import { useConnection, useConnectedWallet } from '@saberhq/use-solana';
 import React, { useEffect } from 'react'
 
 const Playground = () => {
     const connection = useConnection();
     const wallet = useConnectedWallet();
-    const {honeyClient, honeyMarket, honeyUser, honeyReserves} = useMarket(connection, wallet, 'FBm6vYErTiQQbWSqLfJU2CnAGkySBiVL6nQJb5xeCpDL');
+    const {honeyClient, honeyMarket, honeyUser, honeyReserves} = useMarket(connection, wallet, 'GU7mDmGtLXNMo6YsF1FBsrXj2DqnrL82P4eMDKsDPnZZ');
 
     // market returns static object wrappers for now, we can look into customizing them as needed;
     useEffect(()=> {
