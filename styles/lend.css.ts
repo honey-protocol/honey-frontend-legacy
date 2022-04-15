befb4218ad1066a2401297330ab72389af916ecb
Oshioke Iyogwoya <danieloshos3@gmail.com>
2022-04-15 14:03:10 +0100

    responsiveness

--- styles/lend.css.ts
+++ styles/lend.css.ts
@@ -1,9 +1,20 @@
 import { style } from '@vanilla-extract/css';
-import { vars } from 'degen';
+import { breakpoints, vars } from 'degen';
+import { sprinkles } from './theme.css';
 
 export const chartArea = style({
   fontSize: '13px',
-  width: '96%',
+  color: vars.colors.text,
+  fontFamily: vars.fonts.sans,
+  width: '96%', //100% will cause the chart to act up. An issue with recharts
   height: 330,
   marginLeft: '2.5%'
+});
+
+export const actionModuleContainer = style({
+  '@media': {
+    'screen and (max-width: 1280px)': {
+      order: -1
+    }
+  }
 });
