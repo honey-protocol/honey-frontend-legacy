5e3977bd8ebe7007330927937835a8e20f52a734
Oshioke Iyogwoya <danieloshos3@gmail.com>
2022-04-15 14:03:10 +0100

    responsiveness

--- pages/lend/index.tsx
+++ pages/lend/index.tsx
@@ -19,33 +19,33 @@ import * as styles from '../../styles/lend.css';
 const chartData = [
   {
     name: 'Fri',
-    Emmited: 3000
+    APR: 3000
   },
   {
     name: 'Sat',
-    Emmited: 3000
+    APR: 3000
   },
   {
     name: 'Sun',
-    Emmited: 4000
+    APR: 4000
   },
 
   {
     name: 'Mon',
-    Emmited: 4000
+    APR: 4000
   },
   {
     name: 'Tue',
-    Emmited: 4000
+    APR: 4000
   },
   {
     name: 'Wed',
-    Emmited: 5500
+    APR: 5500
   },
 
   {
     name: 'Thu',
-    Emmited: 5500
+    APR: 5500
   }
 ];
 
@@ -87,7 +87,17 @@ const Borrow: NextPage = () => {
           </Box>
         </Stack>
       </Box>
-      <Box display="flex" gap="10" flex={1}>
+      <Stack
+        direction={{
+          xl: 'horizontal',
+          lg: 'vertical',
+          md: 'vertical',
+          sm: 'vertical',
+          xs: 'vertical'
+        }}
+        space="10"
+        flex={1}
+      >
         <Stack direction="vertical" flex={1} space="8">
           <Stack wrap direction="horizontal">
             {cardsDetails.map((detail, i) => (
@@ -109,65 +119,89 @@ const Borrow: NextPage = () => {
               </Box>
             ))}
           </Stack>
-          <Card padding="6" level="2" width="full">
-            <Box marginX="2">
-              <Text size="large" weight="semiBold" color="textPrimary">
-                Supply APR
-              </Text>
+          <Box flex={1} display="flex" alignItems="stretch">
+            <Box
+              borderRadius="3xLarge"
+              backgroundColor="background"
+              paddingX="6"
+              paddingY="10"
+              display="flex"
+              width="full"
+              flex={1}
+            >
+              <Stack space="8" flex={1}>
+                <Box marginX="2">
+                  <Text size="large" weight="semiBold" color="textPrimary">
+                    Supply APR
+                  </Text>
+                </Box>
+                <Box flex={1} display="flex" alignItems="center">
+                  <Box
+                    // flex={1}
+                    className={styles.chartArea}
+                  >
+                    <ResponsiveContainer>
+                      <AreaChart
+                        data={chartData}
+                        margin={{ top: 10, right: 0, left: -20, bottom: 0 }}
+                      >
+                        <defs>
+                          <linearGradient
+                            id="chartGradient"
+                            x1="0"
+                            y1="0"
+                            x2="0"
+                            y2="1"
+                          >
+                            <stop
+                              offset="0%"
+                              stopColor={vars.colors.accent}
+                              stopOpacity={0.24}
+                            />
+                            <stop
+                              offset="100%"
+                              stopColor={vars.colors.accent}
+                              stopOpacity={0}
+                            />
+                          </linearGradient>
+                        </defs>
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip
+                          contentStyle={{
+                            background: '#101115',
+                            border: 'none',
+                            borderRadius: '5px'
+                          }}
+                          cursor={{ strokeWidth: 0.5 }}
+                        />
+                        <Area
+                          type="monotone"
+                          dataKey="APR"
+                          stroke={vars.colors.accent}
+                          strokeWidth={1.7}
+                          fillOpacity={1}
+                          fill="url(#chartGradient)"
+                        />
+                      </AreaChart>
+                    </ResponsiveContainer>
+                  </Box>
+                </Box>
+              </Stack>
             </Box>
-            <Box marginTop="4" className={styles.chartArea}>
-              <ResponsiveContainer>
-                <AreaChart
-                  data={chartData}
-                  margin={{ top: 10, right: 0, left: -20, bottom: 0 }}
-                >
-                  <defs>
-                    <linearGradient
-                      id="chartGradient"
-                      x1="0"
-                      y1="0"
-                      x2="0"
-                      y2="1"
-                    >
-                      <stop
-                        offset="0%"
-                        stopColor={vars.colors.accent}
-                        stopOpacity={0.24}
-                      />
-                      <stop
-                        offset="100%"
-                        stopColor={vars.colors.accent}
-                        stopOpacity={0}
-                      />
-                    </linearGradient>
-                  </defs>
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <Tooltip
-                    contentStyle={{
-                      background: '#101115',
-                      border: 'none',
-                      borderRadius: '5px'
-                    }}
-                    cursor={{ strokeWidth: 0.5 }}
-                  />
-                  <Area
-                    type="monotone"
-                    dataKey="Emmited"
-                    stroke={vars.colors.accent}
-                    strokeWidth={1.7}
-                    fillOpacity={1}
-                    fill="url(#chartGradient)"
-                  />
-                </AreaChart>
-              </ResponsiveContainer>
-            </Box>
-          </Card>
+          </Box>
         </Stack>
-        <Box position="sticky" top="0" display="flex" alignItems="center">
+        <Box
+          position={{ xl: 'sticky', lg: 'relative' }}
+          className={styles.actionModuleContainer}
+          top="0"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
           <DepositWithdrawModule />
         </Box>
-      </Box>
+      </Stack>
     </Layout>
   );
 };
