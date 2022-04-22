bb34f356788fe696389cc3c1c2bb337d85a99975
Oshioke Iyogwoya <danieloshos3@gmail.com>
2022-04-22 15:49:05 +0100

    only staked in filter

--- pages/farm/index.tsx
+++ pages/farm/index.tsx
@@ -1,15 +1,16 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
-import { Box, Text, Stack, Button, IconSearch, Input } from 'degen';
+import { Box, Text, Stack, Button, IconSearch, Input, Spinner } from 'degen';
 import Layout from '../../components/Layout/Layout';
 import * as styles from '../../styles/farm.css';
 import ToggleSwitch from '../../components/ToggleSwitch';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { newFarmCollections, TGFarm } from '../../constants/new-farms';
-import { useConnectedWallet } from '@saberhq/use-solana';
+import { useConnectedWallet, useConnection } from '@saberhq/use-solana';
 import { useWalletKit } from '@gokiprotocol/walletkit';
 import FarmCollectionCard from 'components/FarmCollectionCard/FarmCollectionCard';
 import getCollectionExpireDate from 'helpers/dateUtils';
+import { getFarmsStakedIn } from 'helpers/gemFarm';
 
 /**
  * @params collection and i
@@ -43,9 +44,12 @@ const completedFarms = newFarmCollections.filter((collection, i) => {
 const Farm: NextPage = (props: any) => {
   const wallet = useConnectedWallet();
   const { connect } = useWalletKit();
+  const connection = useConnection();
   const [liveOrCompleted, setLiveOrCompleted] = useState(0);
+  const [allOrStakedIn, setAllOrStakedIn] = useState(0);
   const [searchInput, setSearchInput] = useState('');
   const [displayedCollections, setDisplayedCollections] = useState(liveFarms);
+  const [isLoading, setIsLoading] = useState(false);
 
   /**
    * @params event object
@@ -68,75 +72,136 @@ const Farm: NextPage = (props: any) => {
    * @description sets the UI to live or completed farms
    * @returns sets the farms in the UI
    **/
-  useEffect(() => {
+  const onFarmFilter = useCallback(async () => {
     setSearchInput('');
-    if (!liveOrCompleted) {
+    if (!liveOrCompleted && !allOrStakedIn) {
       setDisplayedCollections(liveFarms);
+    } else if (!liveOrCompleted && allOrStakedIn) {
+      //
+      setIsLoading(true);
+      const stakedInFarms = await getFarmsStakedIn(
+        liveFarms,
+        connection,
+        wallet
+      );
+      if (!stakedInFarms) {
+        setDisplayedCollections([]);
+      } else {
+        setDisplayedCollections(stakedInFarms);
+      }
+      setIsLoading(false);
+      //
+    } else if (liveOrCompleted && allOrStakedIn) {
+      //
+      setIsLoading(true);
+      const stakedInFarms = await getFarmsStakedIn(
+        completedFarms,
+        connection,
+        wallet
+      );
+      if (!stakedInFarms) {
+        setDisplayedCollections([]);
+      } else {
+        setDisplayedCollections(stakedInFarms);
+      }
+      setIsLoading(false);
+      //
     } else {
       setDisplayedCollections(completedFarms);
     }
-  }, [liveOrCompleted]);
+  }, [allOrStakedIn, liveOrCompleted, connection, wallet]);
+
+  useEffect(() => {
+    if (!wallet) return;
+    onFarmFilter();
+  }, [onFarmFilter, wallet]);
 
   return (
     <Layout>
-      <Box>
+      <Box display={'flex'} flexDirection="column" flex={1}>
         <Box minWidth="full" gap="3" paddingTop="4">
-          <Stack direction="horizontal" wrap justify="center" align="center">
-            <Stack
+          <Stack
+            direction="horizontal"
+            wrap
+            justify="space-between"
+            align="center"
+          >
+            {/* <Stack
               direction="horizontal"
               justify="space-between"
               wrap
               align="center"
-            >
-              {/* <Box paddingRight="3">
+            > */}
+            {/* <Box paddingRight="3">
                 <Text align="left" variant="extraLarge" weight="bold">
                   Staking v3 Alpha
                 </Text>
               </Box> */}
+            <ToggleSwitch
+              buttons={[
+                {
+                  title: 'Live',
+                  onClick: () => setLiveOrCompleted(0)
+                },
+                { title: 'Completed', onClick: () => setLiveOrCompleted(1) }
+              ]}
+              activeIndex={liveOrCompleted}
+            />
+            <Stack align="center" direction={'horizontal'}>
               <ToggleSwitch
                 buttons={[
                   {
-                    title: 'Live',
-                    onClick: () => setLiveOrCompleted(0)
+                    title: 'All',
+                    onClick: () => setAllOrStakedIn(0)
                   },
-                  { title: 'Completed', onClick: () => setLiveOrCompleted(1) }
+                  { title: 'Staked In', onClick: () => setAllOrStakedIn(1) }
                 ]}
-                activeIndex={liveOrCompleted}
+                activeIndex={allOrStakedIn}
               />
+              <Box className={styles.searchContainer}>
+                <Input
+                  label=""
+                  value={searchInput}
+                  onChange={onSearchInputChange}
+                  placeholder="Search by name"
+                  prefix={<IconSearch />}
+                />
+              </Box>
             </Stack>
-            <Box className={styles.searchContainer}>
-              <Input
-                label=""
-                value={searchInput}
-                onChange={onSearchInputChange}
-                placeholder="Search by name"
-                prefix={<IconSearch />}
-              />
-            </Box>
           </Stack>
         </Box>
-        <Box minWidth="full" gap="6" paddingTop="8">
-          <Stack>
-            <Box className={styles.collectionCardsContainer}>
-              {/* {[0, 1, 2, 3, 4].map((num, i) => ( */}
-              {displayedCollections.map((item: TGFarm, i: number) => (
-                <Box key={item.id}>
-                  {wallet ? (
-                    <Link href="/farm/[name]" as={`/farm/${item.name}`}>
-                      <a>
+        {isLoading ? (
+          <Box
+            flex={1}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Spinner size="large" />
+          </Box>
+        ) : (
+          <Box minWidth="full" gap="6" paddingTop="8">
+            <Stack flex={1}>
+              <Box className={styles.collectionCardsContainer}>
+                {displayedCollections.map((item: TGFarm, i: number) => (
+                  <Box key={item.id}>
+                    {wallet ? (
+                      <Link href="/farm/[name]" as={`/farm/${item.name}`}>
+                        <a>
+                          <FarmCollectionCard data={item} key={item.id} />
+                        </a>
+                      </Link>
+                    ) : (
+                      <Box onClick={connect} cursor="pointer">
                         <FarmCollectionCard data={item} key={item.id} />
-                      </a>
-                    </Link>
-                  ) : (
-                    <Box onClick={connect} cursor="pointer">
-                      <FarmCollectionCard data={item} key={item.id} />
-                    </Box>
-                  )}
-                </Box>
-              ))}
-            </Box>
-          </Stack>
-        </Box>
+                      </Box>
+                    )}
+                  </Box>
+                ))}
+              </Box>
+            </Stack>
+          </Box>
+        )}
       </Box>
     </Layout>
   );
