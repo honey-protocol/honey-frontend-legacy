9edd0f7a48c3c4fd0d7d60c163e1b803b5aa6a29
Oshioke Iyogwoya <danieloshos3@gmail.com>
2022-04-25 04:32:36 +0100

    change toggle in borrow form

--- components/BorrowNFTsModule/BorrowNFTsModule.tsx
+++ components/BorrowNFTsModule/BorrowNFTsModule.tsx
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
-import { Box, Card } from 'degen';
+import { Box, Card, Stack } from 'degen';
 import LoanBorrow from '../../components/LoanBorrow';
 import LoanRepay from '../../components/LoanRepay';
 import * as styles from './BorrowNFTsModule.css';
 import ToggleSwitchLoan from '../../components/ToggleSwitchLoan';
+import ToggleSwitch from 'components/ToggleSwitch';
 
 interface BorrowNFTsModule {
   NFT: {
-    name: string,
-    image: string,
-    borrowApy: string,
-    estValue: string,
-    assetsBorrowed: number,
-    netBorrowBalance: number,
-    key: number
-  }
+    name: string;
+    image: string;
+    borrowApy: string;
+    estValue: string;
+    assetsBorrowed: number;
+    netBorrowBalance: number;
+    key: number;
+  };
 }
 
 const BorrowNFTsModule = (props: BorrowNFTsModule) => {
@@ -25,24 +26,23 @@ const BorrowNFTsModule = (props: BorrowNFTsModule) => {
   return (
     <Box className={styles.cardContainer}>
       <Card level="2" width="full" padding="8" shadow>
-        <ToggleSwitchLoan
-          buttons={[
-            {
-              title: 'Borrow',
-              onClick: () => setBorrowOrRepay(0)
-            },
-            { title: 'Repay', onClick: () => setBorrowOrRepay(1) }
-          ]}
-          activeIndex={borrowOrRepay}
-        />
-        <hr></hr>
-        {
-          borrowOrRepay == 0  ? (
+        <Stack space="5">
+          <ToggleSwitch
+            buttons={[
+              {
+                title: 'Borrow',
+                onClick: () => setBorrowOrRepay(0)
+              },
+              { title: 'Repay', onClick: () => setBorrowOrRepay(1) }
+            ]}
+            activeIndex={borrowOrRepay}
+          />
+          {borrowOrRepay == 0 ? (
             <LoanBorrow NFT={NFT} />
-            ) : (
-              <LoanRepay NFT={NFT} />
-            )
-        };
+          ) : (
+            <LoanRepay NFT={NFT} />
+          )}
+        </Stack>
       </Card>
     </Box>
   );
