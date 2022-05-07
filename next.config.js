b9d94ccc76664df6481cefc8d76d2f1038e4b779
Bowtiedpyro <bowtiedpyro@Turis-MacBook-Pro.local>
2022-05-07 19:43:40 +0200

    next conf attempt

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const {PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_SERVER} = require('next/consta
 /** @type {import('next').NextConfig} */
 const mainNetEndpoint = process.env.NEXT_PUBLIC_RPC_NODE;
 
+
 /** We should put all environment dependent variables into this file. However, Prod RPC NODE should still reside in
  * .env file for security reason. API Keys and secrets should also reside in .env file
  * "yarn dev" is dev build so by default should use the settings related to  dev-net
@@ -35,17 +36,22 @@ const securityHeaders = [
 module.exports = (phase, {defaultConfig}) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     const env = {
-      // NETWORK: 'devnet',
-      // NETWORK_CONFIGURATION: undefined,
-      // async headers() {
-      //   return [
-      //     {
-      //       // Apply these headers to all routes in your application.
-      //       source: '/(.*)',
-      //       headers: securityHeaders,
-      //     },
-      //   ]
-      NETWORK: 'mainnet-beta',
+      NETWORK: "mainnet-beta",
+      NETWORK_CONFIGURATION: {
+        'mainnet-beta': {
+          name: 'mainnet-beta',
+          endpoint: mainNetEndpoint,
+        }
+      }
+    }
+    const ProdNextConfig = {
+      reactStrictMode: true,
+      env: env,
+    };
+    return withVanillaExtract(ProdNextConfig)
+  } else {
+    const env = {
+      NETWORK: 'devnet',
       NETWORK_CONFIGURATION: {
         'mainnet-beta': {
           name: 'mainnet-beta',
@@ -59,20 +65,7 @@ module.exports = (phase, {defaultConfig}) => {
       env: env,
     };
     return withVanillaExtract(devNextConfig)
-  } else {
-    const env = {
-      NETWORK: 'mainnet-beta',
-      NETWORK_CONFIGURATION: {
-        'mainnet-beta': {
-          name: 'mainnet-beta',
-          endpoint: mainNetEndpoint
-        }
-      }
-    }
-    const ProdNextConfig = {
-      reactStrictMode: true,
-      env: env,
-    };
-    return withVanillaExtract(ProdNextConfig)
   }
 }
+
+
