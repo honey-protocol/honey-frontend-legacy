63242c037953b08757b598b112ffa037387f03b1
Oshioke Iyogwoya <danieloshos3@gmail.com>
2022-05-31 12:28:13 +0100

    add back btn and edited header

--- pages/governance/proposals/index.tsx
+++ pages/governance/proposals/index.tsx
@@ -2,38 +2,55 @@ import { NextPage } from 'next';
 import { useState } from 'react';
 import { ProposalsList } from 'components/Proposals/ProposalsList';
 import Layout from 'components/Layout/Layout';
-import { Button, IconPlus, Stack, Text } from 'degen';
+import { Box, Button, IconChevronLeft, IconPlus, Stack, Text } from 'degen';
 import SmallToggleSwitch from 'components/SmallToggleSwitch/SmallToggleSwitch';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Proposals: NextPage = () => {
   const [showDrafts, setShowDrafts] = useState(true);
+  const router = useRouter();
 
   return (
     <Layout>
-      <Stack
-        align="center"
-        direction="horizontal"
-        space="20"
-        justify="space-between"
-      >
-        <Stack align="center" direction="horizontal">
-          <Text variant="large">All proposals</Text>
-          <Link href="/governance/proposals/create" passHref>
-            <Button size="small" shape="circle">
-              <IconPlus />
-            </Button>
-          </Link>
+      <Box padding="5">
+        <Stack space="8">
+          <Button
+            onClick={router.back}
+            size="small"
+            variant="transparent"
+            rel="noreferrer"
+            prefix={<IconChevronLeft />}
+          >
+            Back
+          </Button>
+          <Stack
+            align="center"
+            direction="horizontal"
+            space="20"
+            justify="space-between"
+          >
+            <Stack space="2" direction="vertical">
+              <Text variant="large" weight="semiBold">
+                All proposals
+              </Text>
+              <Stack align="center" direction="horizontal">
+                <SmallToggleSwitch
+                  isActive={showDrafts}
+                  setIsActive={setShowDrafts}
+                />
+                <Text variant="label">View Drafts</Text>
+              </Stack>
+            </Stack>
+            <Link href="/governance/proposals/create" passHref>
+              <Button variant="secondary" prefix={<IconPlus />} size="small">
+                New proposal
+              </Button>
+            </Link>
+          </Stack>
+          <ProposalsList showDrafts={showDrafts} />
         </Stack>
-        <Stack align="center" direction="horizontal">
-          <SmallToggleSwitch
-            isActive={showDrafts}
-            setIsActive={setShowDrafts}
-          />
-          <Text variant="label">View Drafts</Text>
-        </Stack>
-      </Stack>
-      <ProposalsList showDrafts={showDrafts} />
+      </Box>
     </Layout>
   );
 };
