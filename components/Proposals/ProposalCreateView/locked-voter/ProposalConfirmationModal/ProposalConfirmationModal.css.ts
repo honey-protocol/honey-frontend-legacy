010479d9a35255cff92062de91f456b315fe08c8
Oshioke Iyogwoya <danieloshos3@gmail.com>
2022-06-06 10:57:41 +0100

    fix right overflow on modal

---
+++ components/Proposals/ProposalCreateView/locked-voter/ProposalConfirmationModal/ProposalConfirmationModal.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css';
+
+export const modalInnerBox = style({});
+
+export const modal = style({
+  display: 'grid',
+  maxHeight: '650px',
+  overflow: 'auto'
+});
