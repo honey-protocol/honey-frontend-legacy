375216d6d14de05076defc6bcbd617f915c7b2e6
gonwi <gonzalo.astorgaie@gmail.com>
2022-06-30 13:46:03 +0200

    fix(governance): add production

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,20 +6,59 @@ import '../styles/globals.css';
 import { Network } from '@saberhq/solana-contrib';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { PartialNetworkConfigMap } from "@saberhq/use-solana/src/utils/useConnectionInternal";
+import { accentSequence, ThemeAccent } from 'helpers/theme-utils';
+import { PartialNetworkConfigMap } from '@saberhq/use-solana/src/utils/useConnectionInternal';
+import { useEffect, useState } from 'react';
+import SecPopup from 'components/SecPopup';
+import Script from 'next/script';
 
 const network = process.env.NETWORK as Network;
 const networkConfiguration = () => {
   if (process.env.NETWORK_CONFIGURATION) {
-    return process.env.NETWORK_CONFIGURATION as PartialNetworkConfigMap
+    return process.env.NETWORK_CONFIGURATION as PartialNetworkConfigMap;
   } else {
-    return undefined
+    return undefined;
   }
-}
+};
 
-function MyApp({Component, pageProps}: AppProps) {
+const defaultAccent: ThemeAccent = accentSequence[0];
+const storedAccent =
+  typeof window !== 'undefined'
+    ? (localStorage.getItem('accent') as ThemeAccent)
+    : undefined;
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [showPopup, setShowPopup] = useState(true);
+  const [shouldRender, setShouldRender] = useState(false);
+
+  useEffect(() => {
+    const cautionAgreed = localStorage.getItem('caution-agreed');
+    setShowPopup(cautionAgreed === 'true' ? false : true);
+    setShouldRender(true);
+  }, []);
+
+  if (!shouldRender) return null;
+
   return (
-    <ThemeProvider defaultMode="dark" defaultAccent="red">
+    <ThemeProvider
+      defaultMode="dark"
+      defaultAccent={storedAccent || defaultAccent}
+    >
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
+      />
+
+      <Script id="gtm-script" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA}');
+
+         `}
+      </Script>
       <WalletKitProvider
         defaultNetwork={network}
         app={{
@@ -28,8 +67,14 @@ function MyApp({Component, pageProps}: AppProps) {
         networkConfigs={networkConfiguration()}
       >
         {/* {children} */}
-        <Component {...pageProps} />
-        <ToastContainer theme="dark" position="bottom-right"/>
+        {showPopup ? (
+          <SecPopup setShowPopup={setShowPopup} />
+        ) : (
+          <>
+            <Component {...pageProps} />
+            <ToastContainer theme="dark" position="bottom-right" />
+          </>
+        )}
       </WalletKitProvider>
     </ThemeProvider>
   );
