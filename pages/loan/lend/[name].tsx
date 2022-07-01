2d46223103033c2df17234c65477ab1f7c0083b0
Bowtiedpyro <bowtiedpyro@gmail.com>
2022-07-01 11:02:59 +0300

    adjust package to local instead of npm

--- pages/loan/lend/[name].tsx
+++ pages/loan/lend/[name].tsx
@@ -7,7 +7,7 @@ import {
   withdraw,
   useMarket,
   useHoney,
-} from '@honey-finance/sdk';
+} from '../../../../honey-sdk';
 import { LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
 import Layout from '../../../components/Layout/Layout';
 import DepositWithdrawModule from 'components/DepositWithdrawModule/DepositWIthdrawModule';
