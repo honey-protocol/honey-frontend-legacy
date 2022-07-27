66d00ca6a285564de07a3548499a59e128c4ab81
BowTiedHeron <bowtiedheron@protonmail.com>
2022-07-27 10:09:56 +0200

    changed proposal copy; veHONEY required to activate a proposal and amount of votes required

--- components/Proposals/ProposalCreateView/locked-voter/ProposalCreateInner.tsx
+++ components/Proposals/ProposalCreateView/locked-voter/ProposalCreateInner.tsx
@@ -117,9 +117,9 @@ export const ProposalCreateInner: React.FC = () => {
               <Text as="p">
                 1) Please make sure to start a discussion on the forum to see if there is sufficient support for your proposal.<br />
                 <br />
-                2) Draft proposals can only be activated by a DAO member with at least 1,000,000 $honey, all members of the DAO may vote to execute or reject the proposal.<br />
+                2) Draft proposals can only be activated by a DAO member with at least 10,000 $veHONEY, all members of the DAO may vote to execute or reject the proposal.<br />
                 <br />
-                3) For a proposal to be executed there should be a minimum of 50,000,000 $honey votes casted resulting in at least 2/3 of the votes for one option.
+                3) For a proposal to be executed there should be a minimum of 10,000,000 $veHONEY votes casted resulting in at least 2/3 of the votes for one option.
               </Text>
             </Stack>
           </Card>
