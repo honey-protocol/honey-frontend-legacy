e9c4782c1d20852682a3af5fd7fe7eac780cf97e
Bowtiedpyro <bowtiedpyro@gmail.com>
2022-08-09 21:13:56 +0300

    add comments

--- helpers/loanHelpers/index.tsx
+++ helpers/loanHelpers/index.tsx
@@ -109,7 +109,11 @@ export function BnToDecimal(val: BN | undefined, decimal: number, precision: num
     return 0;
   return val.div(new BN(10 ** (decimal - precision))).toNumber() / (10 ** precision);
 }
-
+/**
+ * @description function which extends the logic as a helper
+ * @params value from SDK | first multiplier | second multiplier
+ * @returns outcome of sum
+*/
 export function BnDivided(val: BN, a: number, b: number) {
   return val.div(new BN(a ** b)).toNumber();
 }
