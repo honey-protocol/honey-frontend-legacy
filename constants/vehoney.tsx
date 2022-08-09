06b2c1f8bcc77b76e2b1085004f463ffc7cd7d0b
daan <danieloshos3@gmail.com>
2022-08-09 08:22:13 +0100

    remove "solana network" for mobile

--- constants/vehoney.tsx
+++ constants/vehoney.tsx
@@ -1,11 +1,9 @@
-export const POOL_INFO_SEED = "PoolInfo";
-export const POOL_USER_SEED = "PoolUser";
-export const LOCKER_SEED = "Locker";
-export const ESCROW_SEED = "Escrow";
+export const POOL_INFO_SEED = 'PoolInfo';
+export const POOL_USER_SEED = 'PoolUser';
+export const LOCKER_SEED = 'Locker';
+export const ESCROW_SEED = 'Escrow';
 export const STAKE_POOL_VERSION = 1;
-export const WHITELIST_ENTRY_SEED = "LockerWhitelistEntry";
+export const WHITELIST_ENTRY_SEED = 'LockerWhitelistEntry';
 
-export const TOKEN_VAULT_SEED = "TokenVault";
-export const VAULT_AUTHORITY_SEED = "VaultAuthority";
-
-export const LOW_TPS = 1700;
+export const TOKEN_VAULT_SEED = 'TokenVault';
+export const VAULT_AUTHORITY_SEED = 'VaultAuthority';
