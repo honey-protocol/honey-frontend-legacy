9a44f53de72360531bed44eb590335d73dc1c6e1
Bowtiedpyro <bowtiedpyro@gmail.com>
2022-08-16 16:51:12 +0300

    loan detail responsive wip

--- styles/name.css.ts
+++ styles/name.css.ts
@@ -12,12 +12,29 @@ export const cardsContainer = style({
 });
 
 export const loanCardsContainer = style({
-  display: 'grid',
-  gridTemplateColumns: '2fr 1fr',
-  gridGap: 20,
+  display: 'flex',
+  flexDirection: 'column',
   '@media': {
     'screen and (min-width: 860px)': {
-      gridTemplateColumns: '2fr 1fr'
+      // gridTemplateColumns: '2fr 1fr',
+      flexDirection: 'row',
+    }
+  }
+});
+
+globalStyle(`${loanCardsContainer} div`, {
+  margin: '0 0 .5em 0',
+  '@media': {
+    'screen and (min-width: 860px)': {
+      // gridTemplateColumns: '2fr 1fr',
+    }
+  }
+});
+
+globalStyle(`${loanCardsContainer} div:first-of-type`, {
+  '@media': {
+    'screen and (min-width: 860px)': {
+      marginRight: '.5em'
     }
   }
 });
@@ -27,4 +44,4 @@ export const flexContainer = style({
 
 globalStyle(`${flexContainer} > div div`, {
   justifyContent: 'space-between'
-})
+});
